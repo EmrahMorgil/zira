e0f9176188ba7b4bbbc8f6df89e47e7a0a54ba01
EmrahMorgil <emrah.morgil@santsg.com>
2024-02-29 15:10:58 +0300

    folder structor update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 export default function Home() {
   return (
     <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, adipisci obcaecati magnam sunt animi soluta repudiandae minus cumque modi nobis delectus recusandae minima quod dolorum commodi repellendus officia architecto dolore?
     </div>
   );
 }
